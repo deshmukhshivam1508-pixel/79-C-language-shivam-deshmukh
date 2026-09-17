db0e7089ba04151f1a7ec83d54df3a3cfab61ddb
deshmukhshivam1508-pixel <deshmukhshivam1508@gmail.com>
2026-09-17 23:01:21 +0530

    Update Logicaloperator.c

--- Lab_03/Logicaloperator.c
+++ Lab_03/Logicaloperator.c
@@ -1,15 +1,14 @@
-#include <stdio.h> 
+#include <stdio.h>
 
- int main()
+int main()
 {
-    int a = 10;
-    int b = 20;
+    int a = 10, b = 20;
 
-    printf("(a < b) && (a != b) = %d", (a < b) && (a != b));
-   
-    printf("(a > b) || (a != b) = %d", (a > b) || (a != b));
-   
-    printf("!(a < b) = %d", !(a < b));
+    printf("(a < b) && (a != b) = %d\n", (a < b) && (a != b));
+ 
+    printf("(a > b) || (a != b) = %d\n", (a > b) || (a != b));
+ 
+    printf("!(a < b) = %d\n", !(a < b));
 
     return 0;
 }
